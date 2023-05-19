473508f0c4a620b99aafc71fc4a94f44e880b7eb
caishihao <2757583135@qq.com>
2023-05-19 16:04:09 +0800

    项目启动

--- src/main/java/com/hao/project/MyApplication.java
+++ src/main/java/com/hao/project/MyApplication.java
@@ -1,5 +1,6 @@
 package com.hao.project;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,10 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.hao.project.mapper")
+@Slf4j
 public class MyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
+        log.info("项目启动成功");
     }
 
 }
